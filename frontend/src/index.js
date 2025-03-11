ebb5283bf276267b805724ace1ea57f8ea194911
lucasemdb <lucas@ebi.ac.uk>
2025-03-11 09:15:49 +0000

    Refactor index.js to use ReactDOM.createRoot and add console logs for debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
+// filepath: /Users/lucas/Development/FreelanceBill/freelance-bill/frontend/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+console.log("Rendering index.js");
+console.log("API URL:", process.env.REACT_APP_API_URL);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
